3ad6605555f6ec7980113f134b2cf7f6675c79a1
Oliver Wilcox <olverbwilcox2@gmail.com>
2021-06-02 15:41:17 +0100

    prog bar update

--- src/App.vue
+++ src/App.vue
@@ -1,132 +1,137 @@
 <template>
-	<div id="app">
-		<img class="breakpack" src="bplogo.png"/>
+  <div id="app">
+    <img class="breakpack" src="bplogo.png" />
 
-        <hr>
-		<p class="about-1">Based in Bristol, UK and Founded by Niall Shaw in 2021, Breakpack is a music platform for a collection of forward-thinking electronic producers, with the aim of connecting more likeminded individuals across the country from different genres, encouraging collaboration to bring out a new underground sound for electronic music.</p>
-        <p class="about-2"> Featuring artists such as short_ghost, Sunbleach, and Spheress.<br>
-		<br>
-Built by Monty Anderson, Oliver Wilcox, and Oliver Redman.</p>
-	  	<div class="releases">
-		  	<Release v-for="r in releases" v-bind:release="r"></Release>
-	  	</div>
-	</div>
+    <hr />
+    <p class="about-1">
+      Based in Bristol, UK and Founded by Niall Shaw in 2021, Breakpack is a
+      music platform for a collection of forward-thinking electronic producers,
+      with the aim of connecting more likeminded individuals across the country
+      from different genres, encouraging collaboration to bring out a new
+      underground sound for electronic music.
+    </p>
+    <p class="about-2">
+      Featuring artists such as short_ghost, Sunbleach, and Spheress.<br />
+      <br />
+      Built by Monty Anderson, Oliver Wilcox, and Oliver Redman.
+    </p>
+    <div class="releases">
+      <Release v-for="r in releases" v-bind:release="r"></Release>
+    </div>
+  </div>
 </template>
 
 <script>
-import Release from './components/Release.vue';
+import Release from "./components/Release.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-	  Release
+    Release,
   },
   data: () => ({
-	  releases: [
-{
-			  id: 'BP-001',
-			  title: 'Mana Man',
-			  cover: 'breakpack.png',
-			  date: '26/05/21',
-			  artist: 'Shortghost',
-              type: 'Single',
-			  spotifylink: 'https://open.spotify.com/album/3kUDCESgS3cT2zBUPpKIlS',
-			  bandcamplink: 'https://shortghost.bandcamp.com/album/mana-man',
-			  track: "https://t4.bcbits.com/stream/c1dcdb689aefb8a7c0e5f92c0894c918/mp3-128/3089568016?p=0&ts=1611411481&t=d9d53628a11d05aec32523cc8acbfbcb5bda83ec&token=1611411481_5f78858d7fe9febec4a0ae2a73903878c4b7292f"
-		  },
-	  ]
-  })
-}
+    releases: [
+      {
+        id: "BP-001",
+        title: "Mana Man",
+        cover: "breakpack.png",
+        date: "26/05/21",
+        artist: "Shortghost",
+        type: "Single",
+        spotifylink: "https://open.spotify.com/album/3kUDCESgS3cT2zBUPpKIlS",
+        bandcamplink: "https://shortghost.bandcamp.com/album/mana-man",
+        track: "",
+      },
+    ],
+  }),
+};
 </script>
 
 <style>
 #app {
-	width: 375px;
-	min-height: 800px;
-	position: absolute;
-	transform: translate(-50%, -50%);
-	left: 50%;
-	top: 400px;
+  width: 375px;
+  min-height: 800px;
+  position: absolute;
+  transform: translate(-50%, -50%);
+  left: 50%;
+  top: 400px;
 }
 
-.breakpack{
-	position: absolute;
-	top: 40px;
-	width: 200px;
-	left: 50%;
-	transform: translateX(-50%);
+.breakpack {
+  position: absolute;
+  top: 40px;
+  width: 200px;
+  left: 50%;
+  transform: translateX(-50%);
 }
 
 h1 {
-	position: absolute;
-	width: 268px;
-	height: 43px;
-	left: 52px;
-	top: 108px;
+  position: absolute;
+  width: 268px;
+  height: 43px;
+  left: 52px;
+  top: 108px;
 
-	font-family: Rubik Mono One;
-	font-style: normal;
-	font-weight: normal;
-	font-size: 35px;
-	line-height: 43px;
+  font-family: Rubik Mono One;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 35px;
+  line-height: 43px;
 
-	color: #FFFFFF;
+  color: #ffffff;
 }
 
 .about-1 {
-	position: absolute;
-	width: 319px;
-	height: 126px;
-	left: 28px;
-	top: 231px;
+  position: absolute;
+  width: 319px;
+  height: 126px;
+  left: 28px;
+  top: 231px;
 
-	font-family: Rubik;
-	font-style: normal;
-	font-weight: normal;
-	font-size: 12px;
-	line-height: 14px;
-	text-align: justify;
+  font-family: Rubik;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 12px;
+  line-height: 14px;
+  text-align: justify;
 
-	color: #FFFFFF;
+  color: #ffffff;
 }
 
 .about-2 {
-	position: absolute;
-	width: 319px;
-	height: 70px;
-	left: 28px;
-	top: 355px;
+  position: absolute;
+  width: 319px;
+  height: 70px;
+  left: 28px;
+  top: 355px;
 
-	font-family: Rubik;
-	font-style: normal;
-	font-weight: normal;
-	font-size: 12px;
-	line-height: 14px;
-	text-align: justify;
+  font-family: Rubik;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 12px;
+  line-height: 14px;
+  text-align: justify;
 
-	color: #FFFFFF;
+  color: #ffffff;
 }
 
 hr {
-	position: absolute;
-	width: 95px;
-	height: 0px;
-	left: 139px;
-	top: 514px;
+  position: absolute;
+  width: 95px;
+  height: 0px;
+  left: 139px;
+  top: 498px;
 
-	border: 2px solid #FFFFFF;
+  border: 2px solid #ffffff;
 }
 
 .releases {
-	position: absolute;
-	top: 578px;
-	height: auto;
-	left: 50%;
-	transform: translateX(-50%);
-width: 313px;
-	height: 50px;
-	
+  position: absolute;
+  top: 569px;
+  height: auto;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 313px;
+  height: 50px;
 }
-
-
 </style>
--- src/components/Release.vue
+++ src/components/Release.vue
@@ -1,107 +1,174 @@
 <template>
-	<div class="release" v-bind:class="{ open }">
-		<hr>
+  <div class="release" v-bind:class="{ open }">
+    <hr />
 
-		<div>
+    <div>
+      <p class="id">{{ release.id }}</p>
+      <p class="title" v-on:click="open = !open">{{ release.title }}</p>
+      <img class="cover" v-bind:src="release.cover" />
+    </div>
 
-			<p class="id" >{{release.id}}</p>
-			<p class="title" v-on:click="open = !open">{{release.title}}</p>
-			<img class="cover" v-bind:src="release.cover">
-		</div>
+    <audio class="playtest" ref="player" v-bind:src="release.track"></audio>
 
-		<audio class="playtest" ref="player" v-bind:src="release.track"></audio>
+    <div class="opencontainer" v-if="open === true">
+      <p class="date">{{ release.date }}</p>
+      <p class="artist">{{ release.artist }}</p>
+      <p class="Type">{{ release.type }}</p>
 
-		<div class="opencontainer" v-if="open === true">
-			<p class="date">{{release.date}}</p>
-			<p class="artist">{{release.artist}}</p>
-			<p class="Type">{{release.type}}</p>
+      <img class="big-cover" v-bind:src="release.cover" />
 
-			<img class="big-cover" v-bind:src="release.cover">
+      <div class="play">
+        <input
+          type="range"
+          class="progress-bar"
+          min="0"
+          max="100"
+          :value="inputCurrentTime"
+          ref="prog"
+          v-on:input="changeProgress()"
+          step="0.1"
+        />
 
-			<div class="play">
-				<div class="line"></div>
-				<p class="title-copy">{{release.title}}</p>
+        <div class="time-left">{{ timeleft }}</div>
+        <div class="line"></div>
+        <div class="title-cont">
+          <p class="title-copy" ref="title">{{ release.title }}</p>
+        </div>
 
-				<p class="playbtn">
-					<svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-pause-fill" viewBox="0 0 16 16" v-if="playing === true" v-on:click="pause">
-  <path d="M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5zm5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5z"/>
-</svg>
-					<svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16" v-else v-on:click="play">
-  <path d="M11.596 8.697l-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/>
-</svg>
-				</p>
+        <p class="playbtn">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="35"
+            height="35"
+            fill="currentColor"
+            class="bi bi-pause-fill"
+            viewBox="0 0 16 16"
+            v-if="playing === true"
+            v-on:click="pause"
+          >
+            <path
+              d="M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5zm5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5z"
+            />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="35"
+            height="35"
+            fill="currentColor"
+            class="bi bi-play-fill"
+            viewBox="0 0 16 16"
+            v-else
+            v-on:click="play"
+          >
+            <path
+              d="M11.596 8.697l-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"
+            />
+          </svg>
+        </p>
+      </div>
+      <a class="spotifybtn" v-bind:href="release.spotifylink" target="_blank">
+        <i class="fab fa-spotify"></i>
+      </a>
 
-				<a class="spotifybtn" v-bind:href="release.spotifylink" target="_blank">
-					<i class="fab fa-spotify"></i>
-				</a>
-
-				<a class="bandcampbtn" v-bind:href="release.bandcamplink" target="_blank">
-					<i class="fab fa-bandcamp"></i>
-				</a>
-			</div>
-		</div>
-
-	</div>
+      <a class="bandcampbtn" v-bind:href="release.bandcamplink" target="_blank">
+        <i class="fab fa-bandcamp"></i>
+      </a>
+    </div>
+  </div>
 </template>
 
 <script>
+//import track from "../yesterday.mp3"
 export default {
-	props: [
-		'release'
-	],
-	data: () => ({
-		open: false,
-		playing: false
-	}),
-	methods: {
-		play() {
-			this.$refs.player.play();
-			this.playing = true;
-		},
-		pause() {
-			this.$refs.player.pause();
-			this.playing = false;
-		}
-	}
+  props: ["release"],
+  data: () => ({
+    open: false,
+    playing: false,
+    inputCurrentTime: 0,
+    timeleft: 0,
+  }),
+  mounted() {
+    window.setInterval(() => {
+      this.setProgress();
+    }, 500);
+  },
+  computed: {
+    progress() {},
+  },
+  methods: {
+    play() {
+      this.$refs.player.play();
+      this.playing = true;
+      this.$refs.title.style.animationPlayState = "initial";
+    },
+    pause() {
+      this.$refs.player.pause();
+      this.playing = false;
+      this.$refs.title.style.animationPlayState = "paused";
+    },
+    changeProgress() {
+      let audioTime =
+        (this.$refs.prog.value / 100) * this.$refs.player.duration;
+      this.$refs.player.currentTime = audioTime;
+    },
+
+    setProgress() {
+      this.inputCurrentTime =
+        (this.$refs.player.currentTime / this.$refs.player.duration) * 100;
+      let audiotimeleft =
+        this.$refs.player.duration - this.$refs.player.currentTime;
+
+      let minutes = `${Math.floor(audiotimeleft / 60)}`;
+
+      if (minutes.length === 1) {
+        minutes = `0${minutes}`;
+      }
+
+      let seconds = `${Math.floor(audiotimeleft % 60)}`;
+      if (seconds.length === 1) {
+        seconds = `0${seconds}`;
+      }
+      this.timeleft = `${minutes}:${seconds}`;
+    },
+  },
 };
 </script>
 
 <style>
 .release {
-	height: 74px;
-	position: relative;
-	transition: 0.2s;
-	
+  height: 74px;
+  position: relative;
+  transition: 0.2s;
 }
 
 .release.open {
-	height: 575px;
+  height: 575px;
 }
 
 .release hr {
-	position: absolute;
-	width: 309px;
-	height: 0px;
-	left: 0px;
-	top: 0px;
+  position: absolute;
+  width: 309px;
+  height: 0px;
+  left: 0px;
+  top: 0px;
 
-	border: 2px solid #FFFFFF;
+  border: 2px solid #ffffff;
 }
 
 .id {
-	position: absolute;
-	width: 80px;
-	height: 24px;
-	left: 0px;
-	top: 26px;
+  position: absolute;
+  width: 80px;
+  height: 24px;
+  left: 0px;
+  top: 26px;
 
-	font-family: Rubik;
-	font-style: normal;
-	font-weight: 500;
-	font-size: 20px;
-	line-height: 24px;
+  font-family: Rubik;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 24px;
 
-	color: #FFFFFF;
+  color: #ffffff;
 }
 
 .title,
@@ -125,122 +192,190 @@ export default {
 }
 
 .cover {
-	position: absolute;
-	width: 30px;
-	height: 30px;
-	right: 0;
-	top: 20px;
+  position: absolute;
+  width: 30px;
+  height: 30px;
+  right: 0;
+  top: 20px;
 }
 
-.date{
-	position: absolute;
-	width: 41px;
-	height: 19px;
-	left: 0px;
-	top: 90px;
+.date {
+  position: absolute;
+  width: 41px;
+  height: 19px;
+  left: 0px;
+  top: 90px;
 
-	font-family: Rubik;
-	font-style: normal;
-	font-weight: 500;
-	font-size: 16px;
-	line-height: 19px;
-	/* identical to box height */
+  font-family: Rubik;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 19px;
+  /* identical to box height */
 
-	color: #FFFFFF;
+  color: #ffffff;
 }
 
 .artist {
-	position: absolute;
-	width: 84px;
-	height: 19px;
-	left: 90px;
-	top: 90px;
-	font-family: Rubik;
-	font-style: normal;
-	font-weight: 500;
-	font-size: 16px;
-	line-height: 19px;
-	color: #FFFFFF;
+  position: absolute;
+  width: 84px;
+  height: 19px;
+  left: 90px;
+  top: 90px;
+  font-family: Rubik;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 19px;
+  color: #ffffff;
 }
 
 .Type {
-	position: absolute;
-	
-	right: 0;
-	top: 90px;
-	font-family: Rubik;
-	font-style: normal;
-	font-weight: 500;
-	font-size: 16px;
-	line-height: 19px;
-	color: #FFFFFF;
+  position: absolute;
+
+  right: 0;
+  top: 90px;
+  font-family: Rubik;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 19px;
+  color: #ffffff;
 }
 
-.play{
-	position: absolute;
-	width: 313px;
-	height: 54px;
-	left: 0px;
-	top: 143px;
+.play {
+  position: absolute;
+  width: 313px;
+  height: 64px;
+  left: 0px;
+  top: 143px;
 
-	background: #FFFFFF;
+  background: #ffffff;
+}
+
+.title-cont {
+  position: absolute;
+  left: 58px;
+  width: 56px;
+  height: 30px;
+  background: rgb(255, 255, 255);
+  overflow: hidden;
 }
 
 .play .title,
-.play .title-copy{
-	top: 15px;
-    color: black;
-	left: 75px;
-	font-size: 18px;
+.play .title-copy {
+  top: 50%;
+  transform: translateY(-50%);
+  color: black;
+  left: 10%;
+  font-size: 18px;
+  animation: scrollText 3s infinite linear;
+  animation-play-state: paused;
+}
 
+@keyframes scrollText {
+  from {
+    left: 10%;
+  }
+  to {
+    left: -200%;
+  }
 }
 
 .line {
-	left:54px;
-	background-color: black;
-	width: 1px;
-	height: 38px;
-	position: absolute;
-	top: 9px;
+  left: 54px;
+  background-color: black;
+  width: 1px;
+  height: 38px;
+  position: absolute;
+  top: 13px;
 }
 
-.playbtn{
-	position:absolute;
-	top: 10px;
-	left: 11px;
-	width: 100px;
-	height: 100px;
-	color: black;
-
+.playbtn {
+  position: absolute;
+  top: 15px;
+  left: 11px;
+  width: 100px;
+  height: 100px;
+  color: black;
 }
 .bi {
   cursor: pointer;
 }
 
-.playbtn .open #pause{
-	display: block;
+.playbtn .open #pause {
+  display: block;
 }
 
 .spotifybtn {
-	position:absolute;
-	top: 15px;
-	left: 245px;
-    font-size: 22px;
-	color: #1DB954;
+  position: absolute;
+  top: 540px;
+  left: 0px;
+  font-size: 24px;
+  color: #1db954;
+  height: 50px;
 }
 
 .bandcampbtn {
-	position:absolute;
-	top: 15px;
-	left: 275px;
-	font-size: 22px;
-	color: #629aa9;
+  position: absolute;
+  top: 540px;
+  left: 30px;
+  font-size: 24px;
+  color: #629aa9;
 }
 
 .big-cover {
-	position: absolute;
-	width: 313px;
-	left: 0px;
-	top: 225px;
+  position: absolute;
+  width: 313px;
+  left: 0px;
+  top: 235px;
+}
+
+.progress-bar {
+  -webkit-appearance: none;
+  appearance: none;
+  position: absolute;
+
+  background: #131313;
+
+  z-index: +1;
+  position: relative;
+  position: relative;
+  top: 20px;
+  left: 120px;
+  width: 170px;
+}
+
+.progress-bar::-webkit-slider-thumb {
+  cursor: pointer;
+  -webkit-appearance: none;
+  margin-top: -3px;
+  margin-left: 0px;
+  height: 9px;
+  width: 9px;
+  border: 0;
+
+  background: black;
+  border-radius: 30px;
+  cursor: pointer;
+}
+.progress-bar::-webkit-slider-runnable-track {
+  appearance: none;
+
+  cursor: pointer;
+  max-height: 3px;
+}
+.progress-bar:focus {
+  outline: none;
+  outline: none;
+}
+
+.time-left {
+  position: absolute;
+  bottom: 10px;
+  right: 22px;
+  font-size: 11px;
+  color: black;
+  font-family: Rubik;
 }
 </style>
